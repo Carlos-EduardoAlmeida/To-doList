b4cef67b5535350220febe18323ef6dfd69bf630
carlos_eduardo <ceduardoeab10@gmail.com>
2023-07-08 18:50:11 -0300

    Fistr

---
+++ script.js
@@ -0,0 +1,44 @@
+//Locais de inserção
+const headerEle = document.body.querySelector("header")
+const inserBotao = document.body.querySelector("p#botao")
+const insercaoBlock = document.body.querySelector("main#insertBlocks")
+
+let contagem = 0
+
+//criando conteúdo header
+const textoHea = document.createElement("h1")
+textoHea.innerText = "Página para prática de DOM e objetos"
+headerEle.appendChild(textoHea)
+
+
+//criando botao
+const botao = document.createElement("button")
+botao.innerText = "Nova tabela"
+botao.addEventListener("click", criarTabela)
+inserBotao.appendChild(botao)
+
+
+//criando Tabelas
+function criarTabela(){
+
+    let textoh = document.body.querySelector("input#textH1")
+    let conTex = textoh.value
+
+    if(Number(conTex.length) > 0){
+        if ( contagem < 8 ){
+            const novaTabela = document.createElement("div")
+            novaTabela.innerHTML = `<h1>${textoh.value}</h1>`
+            novaTabela.innerHTML += `<p>Tabela adicionada, clique novamente para criar outra</p>`
+            insercaoBlock.appendChild(novaTabela)
+            contagem ++
+            console.log(textoh)
+        }else{
+            insercaoBlock.innerHTML = ""
+            contagem = 0
+            alert("Limite de tabelas atingido, vamos reiniciar")
+        }
+    }else{
+        alert("Insira um título")
+    }
+    
+}
